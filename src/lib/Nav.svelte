2ef3647074cc480e6af8f2b336d664d8eeab697e
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2022-08-08 20:09:48 -0500

    First commit

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,44 @@
+
+<nav>
+    <a href="/">
+        <img id="nav-img" class="img-fluid" src="hero.png" alt="Hero">
+    </a>
+    <a href="/">Home</a>
+    <a href="/">Invite</a>
+    <a href="/">Support</a>
+    <a href="/">Dashboard</a>
+
+    <a id="login" href="/login">Login</a>
+</nav>
+
+<style>
+    nav {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        padding: 1rem;
+        height: max-content;
+        position: relative;
+        background-color: #1a1c22;
+    }
+    #nav-img {
+        position: absolute;
+        height: 90%;
+        left: 2rem;
+        top: 0.3rem;
+    }
+    a {
+        color: #FFEFA6;
+        padding: 1rem;
+        transition-duration: 0.3s;
+        font-size: 1.2rem;
+    }
+    a:hover {
+        color: #FFD55F;
+    }
+    a#login {
+        position: absolute;
+        right: 2rem;
+        top: 1rem;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+
+    <!-- class="d-flex flex-column justify-content-center align-items-center" -->
+<header class="container text-center d-flex flex-column justify-content-center align-items-center">
+    <h1 class="p-2">Disbank</h1>
+    <p>Need a fun economy bot to spice up your <br>discord server? You've come to the right place!</p>
+</header>
+
+<style>
+    header {
+        height: 80vh;
+    }
+    header h1 {
+        font-size: 4rem;
+        font-weight: bolder;
+        color: #FFD55F;
+    }
+    header p {
+        color: #bb9c46;
+        font-size: 1.2rem;
+    }
+</style>
